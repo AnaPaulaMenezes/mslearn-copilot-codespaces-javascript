9943161c1ab0d7ed1ec67e4f40be24df33f87d6d
Ana Paula Menezes <39275822+AnaPaulaMenezes@users.noreply.github.com>
2025-08-30 12:42:07 +0000

    Update siteProps with new user information and add bounce animation to social icons

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Ana Paula Menezes",
+  title: "Backend Developer",
+  email: "ana.paula.menezes@example.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "anapaula-menezes",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
